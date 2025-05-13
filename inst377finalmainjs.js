27c67dfb874e78e4ff28dfa5854950dd20dfdd2c
magkuette <153895391+magkuette@users.noreply.github.com>
2025-05-13 02:44:40 -0400

    stuff

---
+++ inst377finalmainjs.js
@@ -0,0 +1,6 @@
+function testFunction() {
+    // document.getElementById("test").innerHTML = document.getElementById("username").value;
+    console.log(document.getElementById("username").value);
+    document.getElementById("test").innerHTML="hiii";
+}
+testFunction();
